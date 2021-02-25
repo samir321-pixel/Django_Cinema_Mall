ff41b3a401094d8bdb4ce55d7ea257e02c98d6e2
Samir <Saitwalsamir@gmail.com>
2021-02-25 21:29:35 +0530

    --Authentication Updated

--- Cinema/user/models.py
+++ Cinema/user/models.py
@@ -9,4 +9,3 @@ class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
     is_employee = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
-   
