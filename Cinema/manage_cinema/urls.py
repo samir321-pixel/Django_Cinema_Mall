d29925039fa86c152c46b4e25170d739e9439dfa
Samir <Saitwalsamir@gmail.com>
2021-02-26 21:50:43 +0530

    --views added

---
+++ Cinema/manage_cinema/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from django.urls import path, include
+
+from .views import *
+
+router = routers.DefaultRouter()
+router.register('cinema', CinemaViewsets, 'cinema')
--- Cinema/manage_cinema/views.py
+++ Cinema/manage_cinema/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+
 
 # Create your views here.
+class CinemaViewsets(viewsets.ModelViewSet):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class CinemaDeckViewsets(viewsets.ModelViewSet):
+    queryset = Cinema_Deck.objects.all()
+    serializer_class = CinemaDeckSerializer
+
+
+class CinemaSlotsDurationViewsets(viewsets.ModelViewSet):
+    queryset = CinemaSlotsDuration.objects.all()
+    serializer_class = CinemaSlotsDurationSerializer
+
+
+class CinemaArrangeSlotViewsets(viewsets.ModelViewSet):
+    queryset = CinemaArrangeSlot.objects.all()
+    serializer_class = CinemaArrangeSlotSerializer
