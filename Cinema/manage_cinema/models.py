8430d75a5c8ce25559a67b9cf631f36aed4a78a6
Samir <Saitwalsamir@gmail.com>
2021-02-26 12:07:43 +0530

    --fields added

--- Cinema/manage_cinema/models.py
+++ Cinema/manage_cinema/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class cinema(models.Model):
+    id = models.AutoField(primary_key=True)
+    movie_name = models.CharField(max_length=200)
+    release_date = models.DateTimeField(auto_now=False)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
