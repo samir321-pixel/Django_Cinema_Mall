e9c050151dc1813e104facf5b253e0a41762dd0d
Samir <Saitwalsamir@gmail.com>
2021-02-26 18:49:10 +0530

    Update models.py

--- Cinema/cinema_booking/models.py
+++ Cinema/cinema_booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Available_Slots(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(auto_now=False)
+
+    def __str__(self):
+        return "{}".format(self.date)
