62870033a90394f57c0e424752117b7545aecf94
Samir <Saitwalsamir@gmail.com>
2021-02-26 18:21:57 +0530

    --cinema model updated

--- Cinema/manage_cinema/models.py
+++ Cinema/manage_cinema/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class cinema(models.Model):
+class Cinema(models.Model):
     id = models.AutoField(primary_key=True)
     movie_name = models.CharField(max_length=200)
     release_date = models.DateTimeField(auto_now=False)
@@ -10,3 +10,6 @@ class cinema(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{}{}".format(self.movie_name, self.release_date)
