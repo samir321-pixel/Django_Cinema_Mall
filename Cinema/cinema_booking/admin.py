d1e21c3d58ee27f0862bec64cc73bc25857d8e92
Samir <Saitwalsamir@gmail.com>
2021-02-27 12:56:15 +0530

    --seats model added

--- Cinema/cinema_booking/admin.py
+++ Cinema/cinema_booking/admin.py
@@ -3,3 +3,4 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Available_Slots)
+admin.site.register(Seat)
--- Cinema/cinema_booking/models.py
+++ Cinema/cinema_booking/models.py
@@ -7,6 +7,21 @@ class Available_Slots(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField(auto_now=False)
     slot = models.ForeignKey("manage_cinema.CinemaArrangeSlot", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return "{}".format(self.date)
+
+
+class Seat(models.Model):
+    id = models.AutoField(primary_key=True)
+    deck = models.ForeignKey("manage_cinema.Cinema_Deck", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    book = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.deck
