525bc8dda838ab7d5a46a6df1b345d3870e95a15
Samir <Saitwalsamir@gmail.com>
2021-02-27 10:05:14 +0530

    --permissions added to manage_cinema

--- Cinema/cinema_booking/models.py
+++ Cinema/cinema_booking/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Available_Slots(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField(auto_now=False)
-    slot = models.ForeignKey("manage_cinema.CinemaArrangeSlot", on_delete=models.CASCADE, null=True, blank=True)
+    slot = models.ForeignKey("manage_cinema.CinemaArrangeSlot", on_delete=models.CASCADE)
 
     def __str__(self):
         return "{}".format(self.date)
--- Cinema/manage_cinema/views.py
+++ Cinema/manage_cinema/views.py
@@ -1,25 +1,29 @@
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class CinemaViewsets(viewsets.ModelViewSet):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CinemaDeckViewsets(viewsets.ModelViewSet):
     queryset = Cinema_Deck.objects.all()
     serializer_class = CinemaDeckSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CinemaSlotsDurationViewsets(viewsets.ModelViewSet):
     queryset = CinemaSlotsDuration.objects.all()
     serializer_class = CinemaSlotsDurationSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CinemaArrangeSlotViewsets(viewsets.ModelViewSet):
     queryset = CinemaArrangeSlot.objects.all()
     serializer_class = CinemaArrangeSlotWriteSerializer
+    permission_classes = (IsAuthenticated,)
